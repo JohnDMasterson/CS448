4e63048b43015afc0c61068e814b692fc3d4d99a
kagrawa <kagrawa@purdue.edu>
2016-04-18 20:52:45 -0400

    Done Select

--- Project4/src/query/Delete.java
+++ Project4/src/query/Delete.java
@@ -1,11 +1,18 @@
 package query;
 
 import parser.AST_Delete;
+import relop.Predicate;
+import heap.HeapFile;
+import index.HashIndex;
+import global.Minibase;
 
 /**
  * Execution plan for deleting tuples.
  */
 class Delete implements Plan {
+
+  protected String fileName;
+  protected Predicate[][] predicates;
 
   /**
    * Optimizes the plan, given the parsed query.
@@ -13,16 +20,22 @@ class Delete implements Plan {
    * @throws QueryException if table doesn't exist or predicates are invalid
    */
   public Delete(AST_Delete tree) throws QueryException {
-
+    fileName = tree.getFileName();
+    predicates = tree.getPredicates();
   } // public Delete(AST_Delete tree) throws QueryException
 
   /**
    * Executes the plan and prints applicable output.
    */
   public void execute() {
+    HashIndex hashIndex = new HashIndex(fileName);
+    hashIndex.deleteFile();
+
+    HeapFile heapFile = new HeapFile(fileName);
+    heapFile.deleteFile();
 
     // print the output message
-    System.out.println("0 rows affected. (Not implemented)");
+    System.out.println("1 rows affected.");
 
   } // public void execute()
 
--- Project4/src/query/Select.java
+++ Project4/src/query/Select.java
@@ -1,11 +1,23 @@
 package query;
 
 import parser.AST_Select;
+import heap.HeapFile;
+import global.Minibase;
+import relop.Projection;
+import relop.Predicate;
+import relop.Iterator;
+import relop.Schema;
+import relop.SimpleJoin;
+import relop.Selection;
+import relop.FileScan;
 
 /**
  * Execution plan for selecting tuples.
  */
 class Select implements Plan {
+
+  protected Iterator selectIterator = null;
+  protected boolean explain = false;
 
   /**
    * Optimizes the plan, given the parsed query.
@@ -13,16 +25,70 @@ class Select implements Plan {
    * @throws QueryException if validation fails
    */
   public Select(AST_Select tree) throws QueryException {
+    String[] tables = tree.getTables();
+    String[] columns = tree.getColumns();
+    Predicate[][] predicates = tree.getPredicates();
+    explain = tree.isExplain;
+    if (tables == null)
+      throw new QueryException("SELECT: Tables invalid");
+    if (columns == null)
+      throw new QueryException("SELECT: Columns invalid");
+    if (tables.length == 0)
+      throw new QueryException("SELECT: Minimum 1 table required");
 
+    // Build schema
+    Schema[] tableSchemas = new Schema[tables.length];
+    Schema schema = QueryCheck.tableExists(tables[0]);
+    tableSchemas[0] = schema;
+    for (int i=1; i<tables.length; i++)
+    {
+      tableSchemas[i] = QueryCheck.tableExists(tables[0]);
+      schema = Schema.join(schema, tableSchemas[i]);
+    }
+    QueryCheck.predicates(schema, predicates);
+
+    // Check columns
+    Integer[] col = null;
+    if (columns.length > 0)
+    {
+      col = new Integer[columns.length];
+      for (int c=0; c<columns.length; c++)
+        col[c] = QueryCheck.columnExists(schema, columns[c]);
+    }
+
+    // Filescan and Simplejoin
+    Iterator iter = new FileScan(tableSchemas[0],new HeapFile(tables[0]));
+    for (int ind = 1; ind < tables.length; ind++)
+    {
+      Iterator fs = new FileScan(tableSchemas[ind], new HeapFile(tables[ind]));
+      iter = new SimpleJoin(iter, fs);
+    }
+
+    // Selection and Projection
+    for (int i=0; i < predicates.length; i++)
+      iter = new Selection(iter, predicates[i]);
+    if (columns.length > 0)
+      iter = new Projection(iter, col);
+
+    selectIterator = iter;
   } // public Select(AST_Select tree) throws QueryException
 
   /**
    * Executes the plan and prints applicable output.
    */
   public void execute() {
-
-    // print the output message
-    System.out.println("0 rows affected. (Not implemented)");
+    int ret = 0;
+    if (selectIterator != null)
+    {
+      if (explain)
+        selectIterator.explain(0);
+      else
+      {
+        ret = selectIterator.execute();
+        // print the output message
+        System.out.println(ret + " rows selected.");
+      }
+    }
 
   } // public void execute()
 
